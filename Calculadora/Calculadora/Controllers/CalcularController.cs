9297a5b6f6535e3c41450305cdae60a528a02778
unknown <Alfredo Ferreira B@AlfredoFerreira.(none)>
2012-08-22 00:17:52 -0400

    SRMD

---
+++ Calculadora/Calculadora/Controllers/CalcularController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculadora.Models;
+
+namespace Calculadora.Controllers
+{
+    public class CalcularController : Controller
+    {
+        //
+        // GET: /Calcular/
+
+        public ActionResult Index()
+        {
+            ViewBag.hora = DateTime.Now.ToString();
+            ViewBag.msn = "Tiempo del Servidor: ";
+            return View();
+        }
+        public ActionResult Suma(Calcular s)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.r = "La SUMA es: " + (s.num1 + s.num2).ToString();
+                return this.View();
+            }
+            return View();
+        }
+        public ActionResult Resta(Calcular r)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.r = "La RESTA es: " + (r.num1 - r.num2).ToString();
+                return this.View();
+            }
+            return View();
+        }
+        public ActionResult Multiplicacion(Calcular m)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.r = "La MULTIPLICACION es: " + (m.num1 * m.num2).ToString();
+                return this.View();
+            }
+            return View();
+        }
+        public ActionResult Division(Calcular d)
+        {
+            if (ModelState.IsValid)
+            {
+                if (d.num2 == 0)
+                {
+                    ViewBag.r = "ERROR...El denominador no puede ser 0 (cero)...";
+                }
+                else
+                {
+                    ViewBag.r = "La DIVISION es: " + (d.num1 / d.num2).ToString();
+                    return this.View();
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Calculadora/Calculadora/Models/Calcular.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Calculadora.Models
+{
+    public class Calcular
+    {
+        [Required]
+        public float num1 { set; get; }
+        [Required]
+        public float num2 { set; get; }
+        public float num3 { set; get; }
+    }
+}
